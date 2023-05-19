50c79deb2be5c351da94b53838eafe44711599b7
Ruben David <rupedavecoo25@gmail.com>
2023-05-19 16:02:32 -0400

    repaso

---
+++ src/components/SegundoComponente.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const SegundoComponente = () => {
+  return (
+    <div>
+        <h2>SegundoComponente</h2>
+        <ul>
+            <li>Dato1</li>
+            <li>Dato2</li>
+            <li>Dato3</li>
+        </ul>
+        
+    </div>
+  )
+}
+
+export default SegundoComponente
